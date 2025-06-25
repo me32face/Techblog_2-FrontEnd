ffc7d15d05b254cdc85c06ea648e4fcb1a0df204
me32face <me32face@gmail.com>
2025-06-25 22:03:52 +0530

    Update  25 June 10PM

--- src/Components/Common-Pages/SummaryBox.jsx
+++ src/Components/Common-Pages/SummaryBox.jsx
@@ -7,20 +7,13 @@ function SummaryBox({ content }) {
   useEffect(() => {
     if (!content) return;
 
-    axios.post(
-    'https://api-inference.huggingface.co/models/facebook/bart-large-cnn',
-    { inputs: content },
-    {
-        headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_HF_TOKEN}`
-        }
-    }
-    ).then(res => {
-      if (res.data && res.data[0] && res.data[0].summary_text) {
+  axios.post(`${process.env.REACT_APP_API_URL}/get-summary`, { content })
+    .then(res => {
+      if (res.data && res.data[0]?.summary_text) {
         setSummary(res.data[0].summary_text);
       }
-    }).catch(err => {
-      console.error("Summary Error:", err);
+    })
+    .catch(() => {
       setSummary("Failed to summarize.");
     });
 
